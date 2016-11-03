62fa62be3a02f4dfaebf55fe785dc7562198dcbe
Joe Coyle <joecoyle@me.com>
2016-11-03 14:07:57 +0000

    Added text to examples

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -4,7 +4,7 @@ var Examples = (props) => {
   return (
     <div>
       <h1 className="text-center page-title">Examples</h1>
-      <p>Here are some example locations to test out</p>
+      <p>Here are some example locations to test out...</p>
       <ol>
         <li><Link to="/?location=Manchester">Manchester</Link></li>
         <li><Link to="/?location=Tokyo">Tokyo</Link></li>
